e11fb2ac3afdfba0da841acc721db08b25e6d970
Maxik <mpoplavskiy38380@gmail.com>
2023-10-22 19:26:21 +0300

    Update website

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Footer from './components/Footer';
 import HomePage from './components/Homepage';
 import Navbar from './components/Navbar';
-import {Route, Routes } from 'react-router-dom';
+import {Navigate, Route, Routes } from 'react-router-dom';
 import Ukraine from './components/Ukraine';
 import Europe from './components/Europe';
 import USA from './components/USA';
@@ -15,6 +15,7 @@ function App() {
       
       <Routes>
         <Route path='/' element={<HomePage />}></Route>
+        <Route path='/gov/' element={<Navigate to="/" />}></Route>
         <Route path='/ukraine' element={<Ukraine />}></Route>
         <Route path='/europe' element={<Europe />}></Route>
         <Route path='/usa' element={<USA />}></Route>
