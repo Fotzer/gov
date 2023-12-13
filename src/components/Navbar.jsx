69dcd3349d05d0378920440be5e93caa41d0c187
Danylo-Pavlusenko <danil2005oleg@gmail.com>
2023-12-13 14:14:07 +0200

    Implemented survey tab

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,6 +16,7 @@ function Navbar(props) {
       <NavLink style={state => ({textDecoration: state.isActive ? "underline" : "none"})} className='navbar-link' to="/Ukraine"><h2 className='navbar-item navbar-region'>{props.language_ua ? "Україна" : "Ukraine"}</h2></NavLink>
       <NavLink style={state => ({textDecoration: state.isActive ? "underline" : "none"})} className='navbar-link' to="/Europe"><h2 className='navbar-item navbar-region'>{props.language_ua ? "Європа" : "Europe"}</h2></NavLink>
       <NavLink style={state => ({textDecoration: state.isActive ? "underline" : "none"})} className='navbar-link' to="/USA"><h2 className='navbar-item navbar-region'>{props.language_ua ? "Америка" : "USA"}</h2></NavLink>
+      <NavLink style={state => ({textDecoration: state.isActive ? "underline" : "none"})} className='navbar-link' to="/Survey"><h2 className='navbar-item navbar-region'>{props.language_ua ? "Опитування" : "Survey"}</h2></NavLink>
 
       <p onClick={setUkrainian} class={props.language_ua ? "navbar_language underline navbar-item first" : "navbar_language navbar-item first"}>Ukrainian</p>
       <p onClick={setEnglish} class={props.language_ua ? "navbar_language navbar-item" : "navbar_language underline navbar-item"}>English</p>
